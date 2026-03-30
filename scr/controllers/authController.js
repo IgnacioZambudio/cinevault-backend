beb0c7acac80124d5be59e291fa5cb60f6fb95cd
Ignacio <ignazambudio@gmail.com>
2026-03-30 20:04:54 +0200

    fix: update requires to correct casing

--- scr/controllers/authController.js
+++ scr/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const generateToken = require("../utils/generateToken");
+const generateToken = require("../utils/generatetoken");
 
 const register = async (req, res) => {
   try {
